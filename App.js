273d7ad0bfaf732e2b091cb34cf38df2ba16bee5
Anurag Pradhan <anuragpradhan@pbpartners.com>
2023-07-14 13:23:23 +0530

    Add Model

--- App.js
+++ App.js
@@ -1,46 +1,72 @@
-import {Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
-import {useState} from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useState } from "react";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [goals, setGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
+  const startAddGoalHandler = () => {
+    setModalIsVisible(true);
+  };
 
-    const [goals, setGoals] = useState([])
+  const closeAddGoalHandler = () => {
+    setModalIsVisible(false);
+  };
 
-    const addGoalHandler = (goal) => {
-        setGoals((prev) =>
-            [...prev, {goal, id: Math.random()}]
-        )
-    }
+  const addGoalHandler = (goal) => {
+    setGoals((prev) => [...prev, { goal, id: Math.random() }]);
+    closeAddGoalHandler();
+  };
 
-    const deleteGoalHandler = (id) => {
-        setGoals((prev) =>
-            prev.filter((goal) => goal.id !== id)
-        )
-    }
+  const deleteGoalHandler = (id) => {
+    setGoals((prev) => prev.filter((goal) => goal.id !== id));
+  };
 
-    return (
-        <View style={styles.appContainer}>
-            <GoalInput onGoalAdd = {addGoalHandler} />
-            <View style={styles.goalsContainer}>
-                <FlatList data={goals} keyExtractor={(item) => item.id}
-                          renderItem={(itemData) => (<GoalItem id = {itemData.item.id} onDelete = {deleteGoalHandler} text={itemData.item.goal}/>)}>
-                </FlatList>
-            </View>
-        </View>
-    )
-
+  return (
+    <View style={styles.appContainer}>
+      <Button title="Add Goal" color="#5e0acc" onPress={startAddGoalHandler} />
+      {modalIsVisible && (
+        <GoalInput
+          onClose={closeAddGoalHandler}
+          isVisible={modalIsVisible}
+          onGoalAdd={addGoalHandler}
+        />
+      )}
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={goals}
+          keyExtractor={(item) => item.id}
+          renderItem={(itemData) => (
+            <GoalItem
+              id={itemData.item.id}
+              onDelete={deleteGoalHandler}
+              text={itemData.item.goal}
+            />
+          )}
+        ></FlatList>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-        paddingTop: 50,
-        paddingHorizontal: 16,
-        flex: 1
-    },
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
 
-    goalsContainer: {
-        flex: 4
-    }
+  goalsContainer: {
+    flex: 4,
+  },
 });
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import {Text, View, StyleSheet, Pressable} from "react-native";
+import React from "react";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 const GoalItem = (props) => {
-    console.log(props.id)
-    const deleteGoalHandler = () => {
-        props.onDelete(props.id)
-    }
-    return (<Pressable onPress={deleteGoalHandler}><View style={styles.goalItem}><Text
-            style={styles.goalTextItem}> {props.text}</Text></View></Pressable>);
+  console.log(props.id);
+  const deleteGoalHandler = () => {
+    props.onDelete(props.id);
+  };
+  return (
+    <Pressable onPress={deleteGoalHandler}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalTextItem}> {props.text}</Text>
+      </View>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
-    goalItem: {
-        marginBottom: 18, padding: 10, backgroundColor: '#8CC0DE', borderRadius: 5,
-    }, goalTextItem: {
-        color: 'white'
-    }
-})
+  goalItem: {
+    marginBottom: 18,
+    padding: 10,
+    backgroundColor: "#311b6b",
+    borderRadius: 5,
+  },
+  goalTextItem: {
+    color: "white",
+  },
+});
 
 export default GoalItem;
